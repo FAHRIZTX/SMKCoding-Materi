472db70f15c72b505a6a0430ef5c63977bc87473
Muhammad Fahri <fahriztx77@gmail.com>
2018-08-25 09:34:02 +0700

    Butter Knife

--- app/src/main/java/me/fahriztx/listview/MainActivity.java
+++ app/src/main/java/me/fahriztx/listview/MainActivity.java
@@ -2,13 +2,27 @@ package me.fahriztx.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn)
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @OnClick(R.id.btn)
+    private void btnClik(Button btn){
+        Toast.makeText(this, "Test", Toast.LENGTH_LONG).show();
+    }
+
 }
 
