ddca620a58886d84b18c36e4254ea8844bcd247c
Muhammad Fahri <fahriztx77@gmail.com>
2018-08-25 11:26:41 +0700

    New

--- app/src/main/java/me/fahriztx/listview/MainActivity.java
+++ app/src/main/java/me/fahriztx/listview/MainActivity.java
@@ -2,12 +2,40 @@ package me.fahriztx.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.frame)
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSatu)
+    public void clickBtnSatu(View btn){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame, new FragmentSatu())
+                .commit();
+    }
+
+    @OnClick(R.id.btnDua)
+    public void clickBtnDua(View btn){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame, new FragmentDua())
+                .commit();
     }
 }
